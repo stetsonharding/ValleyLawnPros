19498a9085dfb4e69c021d42144ece25e92225e1
Stetson Harding <stetson2001@msn.com>
2021-09-22 08:33:21 -0700

    centered columns for mission, responsive.

--- src/components/companyMissionLayout/CompanyMissionLayout.js
+++ src/components/companyMissionLayout/CompanyMissionLayout.js
@@ -5,17 +5,19 @@ import "./CompanyMissionLayout.scss";
 
 //components
 import Mission from "../mission/Mission";
-import MissionImage from "../missionImage/MissionImage";
+// import MissionImage from "../missionImage/MissionImage";
 
 const CompanyMissionLayout = () => {
   return (
-    <Container fluid className="company-mission-layout-container">
-      <Row
-        className="company-mission-layout-row"
-        style={{ backgroundColor: "blue", color: "white" }}
-      >
-        <Col className="text-center"></Col>
-        <Col className="text-center">
+    <Container
+      fluid
+      className="company-mission-layout-container d-flex align-items-center justify-content-center"
+    >
+      <Row className="company-mission-layout-row ">
+        <Col xs={12} lg={6} xl={""} className="text-center">
+          <h1>Hello i am content</h1>
+        </Col>
+        <Col xs={12} lg={4} xl={""} className="text-center">
           <Mission />
         </Col>
       </Row>
