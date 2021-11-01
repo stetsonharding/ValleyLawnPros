7fe13d0238af2a833cc0b950b300f1f5cbe4ff1e
Stetson Harding <stetson2001@msn.com>
2021-10-31 18:44:44 -0700

    resizing on columns for How It Works.

--- src/components/howItWorksLayout/HowItWorksLayout.js
+++ src/components/howItWorksLayout/HowItWorksLayout.js
@@ -8,11 +8,51 @@ function HowItWorksLayout() {
   return (
     <Container
       fluid
-      style={{ height: "600px" }}
+      style={{ minHeight: "600px auto" }}
       className="howItWorks-container d-flex align-items-center justify-content-center"
     >
       <Row>
-        <Col className="d-flex  align-items-center justify-content-center">
+        <Col
+          sm={12}
+          lg={3}
+          className="d-flex  align-items-center justify-content-center"
+        >
+          <LandScapeFlow
+            flowIcon="far fa-comment-dots fa-3x"
+            step="Step 1: we come to you"
+            flowDescription="You'll get a text when we head your way. Our initial meeting is simply to listen to your desires so we can begin 
+            designing a customized plan to fit your budget."
+          />
+        </Col>
+        <Col
+          sm={12}
+          lg={3}
+          className="d-flex  align-items-center justify-content-center"
+        >
+          <LandScapeFlow
+            flowIcon="far fa-comment-dots fa-3x"
+            step="Step 1: we come to you"
+            flowDescription="You'll get a text when we head your way. Our initial meeting is simply to listen to your desires so we can begin 
+            designing a customized plan to fit your budget."
+          />
+        </Col>
+        <Col
+          sm={12}
+          lg={3}
+          className="d-flex  align-items-center justify-content-center"
+        >
+          <LandScapeFlow
+            flowIcon="far fa-comment-dots fa-3x"
+            step="Step 1: we come to you"
+            flowDescription="You'll get a text when we head your way. Our initial meeting is simply to listen to your desires so we can begin 
+            designing a customized plan to fit your budget."
+          />
+        </Col>
+        <Col
+          sm={12}
+          lg={3}
+          className="d-flex  align-items-center justify-content-center"
+        >
           <LandScapeFlow
             flowIcon="far fa-comment-dots fa-3x"
             step="Step 1: we come to you"
