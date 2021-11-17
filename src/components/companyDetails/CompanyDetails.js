e65847d351231322c37fba40c4d44ae190247607
stetsonharding <stetson2001@msn.com>
2021-11-17 07:47:02 -0800

    styling and mobile view organization.

--- src/components/companyDetails/CompanyDetails.js
+++ src/components/companyDetails/CompanyDetails.js
@@ -4,34 +4,22 @@ import "./CompanyDetails.scss";
 
 function CompanyDetails() {
   return (
-    <div
-      className="company-details-container"
-      style={{
-        // backgroundColor: "blueviolet",
-        width: "550px",
-        height: "300px",
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
+    <div className="company-details-container">
+      {/* Details components */}
       <Details detailIcon="fas fa-map-marker-alt" detail="Monroe, Washington" />
-
       <Details detailIcon="fas fa-mobile-alt" detail="(425)330-9709" />
-
       <Details
         detailIcon="far fa-envelope"
         detail="thevalleylawnpros@mail.com"
       />
-      <hr className="new" />
-      <div className="company-details-social" style={{ display: "flex" }}>
-        <i
-          style={{ paddingRight: "10px" }}
-          className="fab fa-instagram fa-2x"
-        ></i>
+      {/* ==== */}
+      <hr className="horizontal-line" />
+      {/* Social Media Icons */}
+      <div className="company-details-social">
+        <i className="fab fa-instagram fa-2x"></i>
         <i className="fab fa-facebook-square fa-2x"></i>
       </div>
+      {/* === */}
     </div>
   );
 }
@@ -39,27 +27,10 @@ function CompanyDetails() {
 //contact details component
 function Details(props) {
   return (
-    <div
-      className="company-details"
-      style={{
-        display: "flex",
-        width: "400px",
-        alignItems: "center",
-        justifyContent: "center",
-        margin: "10px",
-        padding: "10px",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
+    <div className="company-details">
+      <div className="company-details-flex">
         <i className={props.detailIcon + " fa-2x"}></i>
-        <p style={{ padding: "0", margin: "10px" }}>{props.detail}</p>
+        <p>{props.detail}</p>
       </div>
     </div>
   );
--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -9,49 +9,24 @@ import { Container, Row, Col } from "react-bootstrap";
 
 export default function ContactUs() {
   return (
-    <Container fluid style={{ minHeight: "800px" }}>
+    <Container fluid style={{ minHeight: "800px", backgroundColor: "#4b604f" }}>
       <h1 className="contact-title">Contact.</h1>
       <hr className="contact-horizontal-line" />
       <Row>
         <div className="contact-slogan">
           <h4>Let’s talk about your ideas.</h4>
           <p>
-            If you have any questions or querires we will always be happy to
+            If you have any questions or queries we will always be happy to
             help.
           </p>
         </div>
 
         {/* form col */}
-        <Col
-          style={{
-            minHeight: "700px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-          sm={12}
-          md={6}
-          lg={6}
-          xl={6}
-        >
+        <Col className="contact-form-column" sm={12} lg={6}>
           <ContactForm />
         </Col>
         {/* company details col */}
-        <Col
-          style={{
-            margin: "0 auto",
-            minHeight: "700px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          sm={12}
-          sm={12}
-          md={6}
-          lg={6}
-          xl={6}
-        >
+        <Col className="company-details-column" sm={12} lg={6}>
           <CompanyDetails />
         </Col>
       </Row>
