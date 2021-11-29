2b30b792225a03bce9d51725bd4a802c1f856db0
stetsonharding <stetson2001@msn.com>
2021-11-29 09:20:22 -0800

    EmailJS for contact form.

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import emailjs from "emailjs-com";
 
 import "./ContactForm.scss";
 
 export default function ContactForm() {
+  const [contactNotification, setContactNotification] = useState("");
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_mtdoe3h",
+        "template_wymkhcx",
+        e.target,
+        "user_OcADP2ZtNbUvQnfGd1atQ"
+      )
+      .then(
+        (result) => {
+          setContactNotification("Message sent.");
+        },
+        (error) => {
+          setContactNotification(
+            "Error sending your message, please try again."
+          );
+        }
+      );
+
+    e.target.reset();
+  }
+
   return (
     <div className="contact-form-container">
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label></Form.Label>
-          <Form.Control type="text" placeholder="Your Name" />
-          <Form.Label></Form.Label>
-          <Form.Control type="email" placeholder="Your Email" />
+      <Form onSubmit={sendEmail}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="text"
+            name="firstName"
+            placeholder="Name"
+            required
+          />
+
+          <Form.Control
+            className="mt-3"
+            type="tel"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label></Form.Label>
           <Form.Control
+            name="service"
             as="textarea"
             placeholder="We're here for you questions, concerns, and to talk about your ideas!"
             rows={8}
           />
+          <Form.Text className="text-muted">
+            <p style={{ color: "#fff" }}>
+              We will give you a call within the next 24 hours
+            </p>
+            <p style={{ color: "#fff" }}>{contactNotification}</p>
+          </Form.Text>
         </Form.Group>
         <Button
           variant="primary"
